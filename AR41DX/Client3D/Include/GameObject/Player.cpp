7141afdf1d5df07f2e8d0d7131583c842c487d4c
Hyeongho <rlaguddgh@nate.com>
2023-02-06 16:27:30 +0900

    230206
    플레이어 코드 업데이트

--- AR41DX/Client3D/Include/GameObject/Player.cpp
+++ AR41DX/Client3D/Include/GameObject/Player.cpp
@@ -57,28 +57,19 @@ bool CPlayer::Init()
 {
 	CGameObject::Init();
 
-	/*m_Mesh = CreateComponent<CAnimationMeshComponent>("Mesh");
-	m_Camera = CreateComponent<CCameraComponent>("Camera");
-	m_Arm = CreateComponent<CTargetArm>("Arm");
-	m_Rigid = CreateComponent<CRigidBody>("Rigid");
-
-	SetRootComponent(m_Mesh);
-
-	m_Mesh->AddChild(m_Rigid);
-	m_Mesh->AddChild(m_Arm);
-	m_Arm->AddChild(m_Camera);
-
-	m_Camera->SetInheritRotX(true);
-	m_Camera->SetInheritRotY(true);
-
 	m_Arm->SetInheritRotY(true);
 	m_Arm->SetTargetOffset(0.f, 150.f, 0.f);
 
 	m_Mesh->SetMesh("Player");
-	
+
+	m_Mesh->SetMesh("Sandy");
+
 	m_Animation = m_Mesh->SetAnimation<CAnimation>("PlayerAnimation");
 
-	m_Animation->AddAnimation("PlayerIdle", "PlayerIdle", 1.f, 1.f, true);*/
+	//m_Animation->AddAnimation("PlayerIdle", "PlayerIdle", 1.f, 1.f, true);
+	LoadSandyAnim();
+
+	//m_Animation->AddAnimation("PlayerIdle", "PlayerIdle", 1.f, 1.f, true);
 
 	//m_Rigid->SetGravity(true);
 
@@ -114,8 +105,43 @@ void CPlayer::Load(FILE* File)
 	CGameObject::Load(File);
 }
 
+void CPlayer::LoadSpongebobAnim()
+{
+}
+
+void CPlayer::LoadPatrickAnim()
+{
+}
+
+void CPlayer::LoadSandyAnim()
+{
+	m_Animation->AddAnimation("Sandy_Idle", "Sandy_Idle", 1.f, 1.f, true);
+	m_Animation->AddAnimation("Sandy_Walk", "Sandy_Walk", 1.f, 1.f, true);
+	m_Animation->AddAnimation("Sandy_Run", "Sandy_Run", 1.f, 1.f, true);
+	m_Animation->AddAnimation("Sandy_JumpDW", "Sandy_JumpDW", 1.f, 1.f, true);
+	m_Animation->AddAnimation("Sandy_JumpUp", "Sandy_JumpUp", 1.f, 1.f, true);
+	m_Animation->AddAnimation("Sandy_Jump_Landing_NonAdditive", "Sandy_Jump_Landing_NonAdditive", 1.f, 1.f, true);
+	m_Animation->AddAnimation("Sandy_DoubleJump", "Sandy_DoubleJump", 1.f, 1.f, true);
+	m_Animation->AddAnimation("Sandy_Karate_Chop", "Sandy_Karate_Chop", 1.f, 1.f, true);
+	m_Animation->AddAnimation("Sandy_Karate_Kick", "Sandy_Karate_Kick", 1.f, 1.f, true);
+	m_Animation->AddAnimation("Sandy_Lasso_Start", "Sandy_Lasso_Start", 1.f, 1.f, true);
+	m_Animation->AddAnimation("Sandy_Death", "Sandy_Death", 1.f, 1.f, true);
+}
+
 void CPlayer::MoveFront()
 {
+	switch (m_MainCharacter)
+	{
+	case EMain_Character::Spongebob:
+		break;
+	case EMain_Character::Patrick:
+		break;
+	case EMain_Character::Sandy:
+		break;
+	default:
+		break;
+	}
+
 	AddWorldPosition(GetWorldAxis(AXIS_Z) * -100.f * CEngine::GetInst()->GetDeltaTime());
 }
 
@@ -126,15 +152,51 @@ void CPlayer::MoveBack()
 
 void CPlayer::MoveLeft()
 {
+	switch (m_MainCharacter)
+	{
+	case EMain_Character::Spongebob:
+		break;
+	case EMain_Character::Patrick:
+		break;
+	case EMain_Character::Sandy:
+		break;
+	default:
+		break;
+	}
+
 	AddWorldRotationY(180.f * CEngine::GetInst()->GetDeltaTime());
 }
 
 void CPlayer::MoveRight()
 {
+	switch (m_MainCharacter)
+	{
+	case EMain_Character::Spongebob:
+		break;
+	case EMain_Character::Patrick:
+		break;
+	case EMain_Character::Sandy:
+		break;
+	default:
+		break;
+	}
+
+	AddWorldRotationY(-180.f * CEngine::GetInst()->GetDeltaTime());
 }
 
 void CPlayer::Jump()
 {
+	switch (m_MainCharacter)
+	{
+	case EMain_Character::Spongebob:
+		break;
+	case EMain_Character::Patrick:
+		break;
+	case EMain_Character::Sandy:
+		break;
+	default:
+		break;
+	}
 }
 
 void CPlayer::AttackKey()
@@ -155,5 +217,35 @@ void CPlayer::CameraRotationKey()
 	if (MouseMove.y != 0.f)
 	{
 		m_Arm->AddRelativeRotationX(MouseMove.y * DeltaTime * 180.f);
+	}
+}
+
+void CPlayer::ChangeSpongebob()
+{
+	if (CResourceManager::GetInst()->FindMesh("Spongebob"))
+	{
+		m_Mesh->SetMesh("Spongebob");
+
+		m_MainCharacter = EMain_Character::Spongebob;
+	}
+}
+
+void CPlayer::ChangePatrick()
+{
+	if (CResourceManager::GetInst()->FindMesh("Patrick"))
+	{
+		m_Mesh->SetMesh("Patrick");
+
+		m_MainCharacter = EMain_Character::Patrick;
+	}
+}
+
+void CPlayer::ChangeSandy()
+{
+	if (CResourceManager::GetInst()->FindMesh("Sandy"))
+	{
+		m_Mesh->SetMesh("Sandy");
+
+		m_MainCharacter = EMain_Character::Sandy;
 	}
 }
