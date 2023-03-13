db4fd23bc9cee95dd5d96bdd1eddcf070f73530d
ArgoMel <122796999+ArgoMel@users.noreply.github.com>
2023-03-13 13:45:50 +0900

    230313 김범중
    엔진
    RenderManager 파티클 로드 수정
    
    에디터
    KingJellyfishSceneInfo 추가
    JellyfishFieldSceneInfo 수정
    EditorManager 타이틀신으로 고정

--- AR41DX/AR41Editor/Include/EditorManager.cpp
+++ AR41DX/AR41Editor/Include/EditorManager.cpp
@@ -27,6 +27,8 @@
 #include "Scene/BikiniCitySceneInfo.h"
 #include "Scene/TitleSceneInfo.h"
 #include "Scene/TestScene.h"
+#include "Scene/JellyfishFieldSceneInfo.h"
+#include "Scene/KingJellyfishSceneInfo.h"
 
 CEditorManager::CEditorManager()
 {
@@ -50,10 +52,14 @@ bool CEditorManager::Init(HINSTANCE hInst)
     //CSceneInfo* Info = new CEditorDefaultScene;
     CSceneInfo* Info = new CTitleSceneInfo;
     //CSceneInfo* Info = new CBikiniCitySceneInfo;
+    //CSceneInfo* Info = new CJellyfishFieldSceneInfo;
+    //CSceneInfo* Info = new CKingJellyfishSceneInfo;
 
     //CScene::AddSceneInfoCDO("EditorDefaultScene", Info);
     CScene::AddSceneInfoCDO("TitleSceneInfo", Info);
     //CScene::AddSceneInfoCDO("BikiniCitySceneInfo", Info);
+    //CScene::AddSceneInfoCDO("JellyfishFieldSceneInfo", Info);
+    //CScene::AddSceneInfoCDO("KingJellyfishSceneInfo", Info);
 
     CEngineShareSetting::Setting();
 
@@ -75,6 +81,8 @@ bool CEditorManager::Init(HINSTANCE hInst)
    // CSceneManager::GetInst()->CreateSceneInfo<CEditorDefaultScene>();
     CSceneManager::GetInst()->CreateSceneInfo<CTitleSceneInfo>();
     //CSceneManager::GetInst()->CreateSceneInfo<CBikiniCitySceneInfo>();
+    //CSceneManager::GetInst()->CreateSceneInfo<CJellyfishFieldSceneInfo>();
+    //CSceneManager::GetInst()->CreateSceneInfo<CKingJellyfishSceneInfo>();
 
     CObjectWindow* objectWindow = CEditorGUIManager::GetInst()->FindEditorWindow<CObjectWindow>("ObjectWindow");
     if (objectWindow)
---
+++ AR41DX/AR41Editor/Include/Scene/KingJellyfishSceneInfo.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Scene\SceneInfo.h"
+
+class CKingJellyfishSceneInfo : public CSceneInfo
+{
+public:
+    CKingJellyfishSceneInfo();
+    ~CKingJellyfishSceneInfo();
+    virtual bool Init();
+};
+
