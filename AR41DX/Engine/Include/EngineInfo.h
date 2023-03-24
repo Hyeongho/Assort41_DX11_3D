69c57459ad302b1f67394b0c376b3dd7180db946
Hyeongho <rlaguddgh@nate.com>
2023-03-24 20:21:32 +0900

    230324 김형호
    recast 네비매쉬 관련 해더파일, 소스파일 추가

--- AR41DX/Engine/Include/EngineInfo.h
+++ AR41DX/Engine/Include/EngineInfo.h
@@ -33,8 +33,30 @@
 #include "Resource/Texture/DirectXTex.h"
 #include "fmod.hpp"
 
+#include "Recast.h"
+#include "RecastAlloc.h"
+#include "RecastAssert.h"
+#include "DetourAlloc.h"
+#include "DetourAssert.h"
+#include "DetourCommon.h"
+#include "DetourNavMesh.h"
+#include "DetourNavMeshBuilder.h"
+#include "DetourNavMeshQuery.h"
+#include "DetourStatus.h"
+#include "DetourNode.h"
+#include "DetourMath.h"
+
 #pragma comment(lib, "../Bin/fmod64_vc.lib")
 
+//#ifdef _DEBUG
+//
+//#pragma comment(lib, "../Bin/Recast-d.lib")
+//
+//#else
+//
+//#pragma comment(lib, "../Bin/Recast.lib")
+//
+//#endif // _DEBUG
 
 extern float g_DeltaTime;
 
