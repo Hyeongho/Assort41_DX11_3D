720b22cc03c10d53af95de797c031560a6507c93
ArgoMel <122796999+ArgoMel@users.noreply.github.com>
2023-04-05 11:52:45 +0900

    230405 김범중
    엔진
    CScene 스카이텍스처 없을때 예외처리
    
    에디터
    SpongebobMissile 크로스헤어 추가
    Player 사운드 예외처리

--- AR41DX/AR41Editor/Include/GameObject/Player.cpp
+++ AR41DX/AR41Editor/Include/GameObject/Player.cpp
@@ -69,6 +69,13 @@ CPlayer::CPlayer(const CPlayer& Obj)
 
 CPlayer::~CPlayer()
 {
+	if (m_Scene)
+	{
+		m_Scene->GetResource()->SoundStop("BikiniBottom");
+		m_Scene->GetResource()->SoundStop("JellyfishField");
+		m_Scene->GetResource()->SoundStop("BossStage");
+		m_Scene->GetResource()->SoundStop("BossStage");
+	}
 	if (m_LoadData != m_PlayerData)
 	{
 		SaveCharacter();
